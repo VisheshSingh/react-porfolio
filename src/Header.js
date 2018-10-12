0344df6eebe1f92621e4dc9e59121974e3e5c13a
Vishesh <visheshsingh16@gmail.com>
2018-10-12 12:39:28 -0400

    Header component with content

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import "./Header.css";
 import Background from "./img/heroImage.jpg";
 
 class Header extends Component {
@@ -9,7 +9,17 @@ class Header extends Component {
       height: "70vh",
       backgroundSize: "cover"
     };
-    return <header style={myStyles} />;
+    return (
+      <header style={myStyles}>
+        <div class="content">
+          <h1>{this.props.mainTitle}</h1>
+          <p>Take a glimpse at my professional work....</p>
+          <a href="#findMore" class="btn">
+            {this.props.button}
+          </a>
+        </div>
+      </header>
+    );
   }
 }
 
